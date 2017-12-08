ed663d14c900575eb25b0959e9dc8314b2fb4dc1
zhuqiang <zhuqiang@tidebuy.net>
2017-12-08 10:30:16 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # newstudy
-新的计划学习仓库包
+旧的仓库：https://gitee.com/zhuqiang/study
+
+随着学习的越来越多，在学习的过程中发现并不是所有的学习过程中的代码都是没有价值的。
+
+该仓库用来存储学习的项目；下表是每个项目的说明；
+
+要严格要求自己做好分类，因为数量多的时候分类就很重要。
+
+| 项目 | 描述 |
+|--------------
+| java-tutorial |  [官网教程阅读笔记](https://www.gitbook.com/book/zq99299/java-tutorial/details) 过程实践代码，笔记
---
+++ temp-test/src/main/java/cn/mrcode/newstudy/temptest/Test.java
@@ -0,0 +1,15 @@
+package cn.mrcode.newstudy.temptest;
+
+/**
+ * <pre>
+ *  Version         Date            Author          Description
+ * ---------------------------------------------------------------------------------------
+ *  1.0.0           2017/12/08     zhuqiang        -
+ * </pre>
+ * @author zhuqiang
+ * @version 1.0.0 2017/12/8 10:27
+ * @date 2017/12/8 10:27
+ * @since 1.0.0
+ */
+public class Test {
+}

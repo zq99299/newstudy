c7d8e4687a170bd818bd43c32df64edc52c76fbc
zhuqiang <99299684@qq.com>
2018-03-02 23:31:57 +0800

    快速排序测试

---
+++ hp-base/src/test/java/cn/mrcode/newstudy/hpbase/_01/q07/Practice07.java
@@ -0,0 +1,23 @@
+package cn.mrcode.newstudy.hpbase._01.q07;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author : zhuqiang
+ * @version : V1.0
+ * @date : 2018/3/2 22:35
+ */
+public class Practice07 {
+    // 快速排序
+    @Test
+    public void quickSortTest() {
+        int[] arr = {6, 8, 1, 3, 2, 6, 4};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        QuickSort.sort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+    }
+
+
+}

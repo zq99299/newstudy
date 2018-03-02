8d7b68826e3ac37c667bd6fc9e4b27326b72f624
zhuqiang <99299684@qq.com>
2018-03-02 23:43:04 +0800

    冒泡排序

--- hp-base/src/test/java/cn/mrcode/newstudy/hpbase/_01/q07/Practice07.java
+++ hp-base/src/test/java/cn/mrcode/newstudy/hpbase/_01/q07/Practice07.java
@@ -19,5 +19,24 @@ public class Practice07 {
         System.out.println("排序后：" + Arrays.toString(arr));
     }
 
+    @Test
+    public void bubblingTest() {
+        int[] arr = {6, 8, 1, 3, 2, 6, 4};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        bubblingSort(arr);
+        System.out.println("排序后：" + Arrays.toString(arr));
+    }
 
+    public void bubblingSort(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length; j++) {
+                int temp = arr[i];
+                if (temp > arr[j]) {
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
 }

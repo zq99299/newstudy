5a09bebb1aac86dd9723cae18253efb353a20ee4
zhuqiang <99299684@qq.com>
2018-03-11 13:04:35 +0800

    增加克隆方式创建并解析对象

--- hp-base/src/test/java/cn/mrcode/newstudy/hpbase/_01/Salary.java
+++ hp-base/src/test/java/cn/mrcode/newstudy/hpbase/_01/Salary.java
@@ -5,7 +5,9 @@ package cn.mrcode.newstudy.hpbase._01;
  * @version : V1.0
  * @date : 2018/3/1 21:48
  */
-public class Salary {
+public class Salary implements Cloneable {
+    // 用于克隆降低new构造
+    private static Salary cloneSalary = new Salary();
     private String name;
     private int baseSalary;
     private int bonus;
@@ -37,5 +39,29 @@ public class Salary {
 
     public void setBonus(int bonus) {
         this.bonus = bonus;
+    }
+
+    /**
+     * 解析一行数据,并返回对象
+     * @param line
+     * @return
+     */
+    public static Salary parse(String line) {
+        try {
+            String[] salaryPart = line.split("\t");
+            String name = salaryPart[0];
+            int baseSalary = Integer.valueOf(salaryPart[1]);
+            int bonus = Integer.valueOf(salaryPart[2]);
+
+            Salary newSalary = (Salary) cloneSalary.clone();
+//            Salary newSalary = new Salary();
+            newSalary.setName(name);
+            newSalary.setBaseSalary(baseSalary);
+            newSalary.setBonus(bonus);
+            return newSalary;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
